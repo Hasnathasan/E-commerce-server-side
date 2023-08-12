aca04b7e5921c4a5a2b4d17a2844516a9acaee18
Hasnathasan <hasnatoooooooo@gmail.com>
2023-08-12 18:23:48 +0600

    api added to check userRole

--- index.js
+++ index.js
@@ -80,6 +80,14 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/userRole', async(req, res) => {
+      const email = req.query.email;
+      const query = {email: email};
+      console.log(email);
+      const result = await usersCollection.findOne(query);
+      res.send(result?.role)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
